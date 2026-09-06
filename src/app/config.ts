4823840cc713c43c2a03bebecae54d42fd91d07a
Francisco Cardozo <fcardozo199@gmailcom>
2026-09-05 23:29:50 -0500

    update endpoint

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export default {
   invokerApiEndpoint: 'https://7ltdf1rlv6.execute-api.us-east-1.amazonaws.com/prod',
-  statusApiEndpoint: '',
+  statusApiEndpoint: 'https://00aux9far9.execute-api.us-east-1.amazonaws.com/prod',
   evidencesUrl: 'https://dx00tjeeew18t.cloudfront.net',
   url: 'https://d1c3vouxlq3170.cloudfront.net',
   serviceEndpoints: {
